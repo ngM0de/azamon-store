d9f49bc238efd4acb19926e689bd0d3db2074e38
ngM0de <tillic0llapse@protonmail.com>
2024-02-25 00:36:53 +0200

    feat:clear default frontend generated content

--- apps/front/src/app/app.component.ts
+++ apps/front/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'azamon-store-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'front';
 }
